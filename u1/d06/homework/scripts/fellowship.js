c12b9f5efe0227d76610f2c80b9382d9bfb4a382
Jared <jaredcmurphy@gmail.com>
2016-10-24 14:03:33 -0400

    add lotr hw

---
+++ u1/d06/homework/scripts/fellowship.js
@@ -0,0 +1,25 @@
+console.log("Linked.");
+
+// Our heros
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
+
+// Their compatriots
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+
+
+// Their journey
+var lands = ['The Shire', 'Rivendell', 'Mordor'];
+
+// An example of using query selector
+var body = document.querySelector('body');
