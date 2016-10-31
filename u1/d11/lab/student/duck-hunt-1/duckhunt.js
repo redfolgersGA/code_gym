781017c07468dd17f68821bbb754abe42ab71ea4
Bryan Mytko <bryanmytko@gmail.com>
2016-10-31 10:27:45 -0400

    Add u1/d11 duck-hunt-1

---
+++ u1/d11/lab/student/duck-hunt-1/duckhunt.js
@@ -0,0 +1,18 @@
+// This is a jQuery "ready" closure:
+jQuery(function() {
+  // - Unlike a normal closure, we don't have to call it
+  // - jQuery will call it when the page is loaded
+
+  // I'll do some of the work for you, first I will grab the <body></body>
+  var body = $('body');
+
+  // 1. Can you create a <div> with the class "duck" and name it "duck"
+
+  // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
+
+  // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left"
+
+  // 4. Try making the duck move to a different location after 1 second
+
+  // 5. Congratulations!
+})
