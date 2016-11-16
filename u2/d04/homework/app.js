8200a0926c0ee216277be5977b90862975db4bd1
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2016-11-15 19:13:00 -0500

    add step 4 of d04 hw

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -1,2 +1,15 @@
 var http = require("http");
 var fs = require("fs");
+
+var port = 8080;
+server.listen(port);
+console.log("listening on port ", port);
+
+var getHTML = function(res){
+  res.writeHead(200,{"Content-type": "text/html"});
+  var html = fs.readFileSync(__dirname + "/public/index.html");
+  res.end(html);
+
+}
+
+
