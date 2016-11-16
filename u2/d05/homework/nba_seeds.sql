0a1592f85cf89165df97895d525dbc8dc0f153df
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-16 12:09:17 -0500

    add hw

--- u2/d05/homework/nba_seeds.sql
+++ u2/d05/homework/nba_seeds.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS players;
 -- add insert player here
 
 
--- add copy players here (with YOUR file path!)
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/Jared/pangolins/u2_the_server/d24_psql/homework/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+-- this needs to include YOUR PATH!! YOURS! NOT THE PATH LISTED BELOW!
+COPY players
+  (name, age, team, games, points)
+FROM '/Users/Jared/pangolins/u2_the_server/d24_psql/homework/nba_season_2011_2012.csv'
+    DELIMITER ',' CSV;
