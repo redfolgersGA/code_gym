6e34e979d645880a455c95432abcb379b861aa8f
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-18 01:13:56 -0500

    update lesson directory

---
+++ u2/d07/pg-promise/beer_list/db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO beers (name, category, country, alcohol, price) VALUES ('Michelob Ultra', 'American Lager', 'USA', '4.2', '5.00');
+INSERT INTO beers (name, category, country, alcohol, price) VALUES ('Heineken', ' European Lager', 'Germany', '5.0', '5.00');
+INSERT INTO beers (name, category, country, alcohol, price) VALUES ('Blue Moon', 'Belgian Ale', 'United States', '5.4', '7.00');
+INSERT INTO beers (name, category, country, alcohol, price) VALUES ('Stella Artois', 'European Lager', 'Belgium', '5.2', '6.00');
