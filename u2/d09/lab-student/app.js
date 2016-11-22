09808e8c1510e763e21f5e51097b6ebbb550cf42
Bryan Mytko <bryanmytko@gmail.com>
2016-11-22 11:19:26 -0500

    Add u2/d09 bcrypt lab

---
+++ u2/d09/lab-student/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')();
+const mustacheExpress = require('mustache-express');
+const bodyParser = require("body-parser");
+const session = require('express-session');
+
+/* BCrypt stuff here */
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use("/", express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'theTruthIsOutThere51',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+var db = pgp('postgres://xxxxxxxxxxxxxxxxxxxxxxxxxxx@localhost:5432/auth_xfiles');
+
+app.listen(3000, function () {
+  console.log('Pangolins Auth App: listening on port 3000!');
+});
