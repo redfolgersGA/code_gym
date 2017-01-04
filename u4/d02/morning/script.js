54ca5bfe823e8cb1df0240a2820aeb3090bcc28c
heidipowers <heidipowers@gmail.com>
2017-01-04 09:00:13 -0500

    morning ex

---
+++ u4/d02/morning/script.js
@@ -0,0 +1,25 @@
+//1. Create the Pre-ES6 pet object
+
+//2. Create the ES6 Pet class
+
+//3. Create Dog, Cat, and Fish classes that inherit from the Pet class
+
+//4. Create a new instance of the Dog, Cat, and Fish Class
+
+//5. Run the following commands on the instances created in 4:
+
+//  Feed the dog twice, then check it's hunger.
+
+//  Increase the dog's age twice, then check how old it is.
+
+//  Make the dog bark.
+
+//  Make the dog go outside.
+
+//  Make the cat meow.
+
+//  Make the cat use the litter box.
+
+//  Give the fish medicine twice, then check it's health.
+
+//  Make the fish swim.
