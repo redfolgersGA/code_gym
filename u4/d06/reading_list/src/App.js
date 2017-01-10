27ebada079eac0f309bd4b99ef3515275233c2ef
Jared Murphy <jaredcmurphy@gmail.com>
2017-01-10 09:24:59 -0500

    update things

--- u4/d06/reading_list/src/App.js
+++ u4/d06/reading_list/src/App.js
@@ -3,15 +3,14 @@ import BookList from './book-list';
 import BookDetail from './book-detail';
 
 export default class App extends Component {
-  componentWillMount = () => {
-    this.setState({
+  constructor(){
+    super();
+    this.state = {
       currentBook: null
-    })
+    };
   }
   
   render() {
-
-    // pass the selectBook function as props down to the bookList component once you're ready
     return (
       <div>
         <BookList selectBook={this.selectBook.bind(this)} />
@@ -24,6 +23,6 @@ export default class App extends Component {
   selectBook = (book) => {
     this.setState({
       currentBook: book
-    })
+    });
   }
 }
