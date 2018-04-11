a91eac3ef83200166987f4538b422bc44811a3b2
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-04-11 09:30:34 -0400

    add

--- u1/d10/morning/scripts/main.js
+++ u1/d10/morning/scripts/main.js
@@ -5,19 +5,21 @@ $(document).ready(function(){
 
 // 2. Query the DOM for the h1 and store it in a sensibly named variable.
 var $laSucks = $('h1');
+
 // 3. Change the heading's text so that it reads "Inhabitants of Los Angeles (1984)".
-$('h1').replaceWith('Los Angeles Sucks in 1984 and Forever');
+$($laSucks).replaceWith('Inhabitants of LA (1984');
 // 4. Query the DOM for any list items with the class of cop, and store the elements in a variable called cops.
-$('li' .cop')
-var $cops = $('.cop')
+var $cops = $('li.cop')
+
 
 // 5. Make all the cops' names the color of lightskyblue.
-$cops.css('color', 'lightskyblue')
+$cops.css('color', 'red')
 // 6. Create a div with the class of terminator, and append it to the div with the id of los-angeles.
-var $termin = $('<div></div>')
-$('body').append($termin)
-$termin.addClass('terminator')
-$termin.attr('id', 'los-angles')
+var $termin = $('<div class="terminator"></div>')
+var $divLA = $('div#los-angeles.main')
+
+$termin.appendTo($divLA)
+
 // 7. Query the DOM for the element with id of the-one-true-sarah-connor, and save it in a variable called theOneTrueSarahConnor.
 var $oneTrue = $('#the-one-true-sarah-connor');
 // 8. Give theOneTrueSarahConnor the class of badass.
@@ -27,17 +29,23 @@ var $poorSouls = $('.collateral-damage');
 // 10. Remove poorSouls from the DOM one by one.
 //you need to use your own variable names in place of the ones down here. this is an example
 
-$poorSouls.each(function(i, eachItem) {
-  var el = $(li);
-  el.delay(500 * i).fadeOut(2000);
+function removeSouls(){
+  $poorSouls.each(function(i){
+    $(this).remove()
 
-});
+    setTimeout(1000)
+  })
+}
+setTimeout(removeSouls, 4000)
 
-var $poorSouls = $('.collateral-damage');
-$poorSouls.each(function(i, li) {
-  console.log(i)
-  var el = $(li);
-  el.delay(500 * i).fadeOut(200);
+
+
+
+// var $poorSouls = $('.collateral-damage');
+// $poorSouls.each(function(i, li) {
+//   console.log(i)
+//   var el = $(li);
+//   el.delay(500 * i).fadeOut(200);
   });
 
 
