848ba7ef18f1c97888a2012765c9c5d2df547f15
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-04-12 11:36:32 -0400

    building image slider

--- PORTFOLIO/scripts/script.js
+++ PORTFOLIO/scripts/script.js
@@ -1,8 +1,20 @@
 $("document").ready(function(){
   console.log("jquery is loaded")
 
+var $img2 = $('div#img2')
+$($img2).hide()
+
+function switchImage() {
+  var $img1 = $('div#img1')
+  var $img2 = $('div#img2')
+  $($img2).hide()
+  $($img1).hide()
+  $($img2).show()
+  $(img1).attr('src', 'img/Headshot.jpg')
+}
 
 
+setInterval(switchImage, 2000)
 
 
 
