924ce4e4ee82a058ec178d08d058e0762cc93a97
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-04-25 13:37:31 -0400

    name li dynamically changes the #user_title p tag

--- u2/d02/ajax_lesson/script.js
+++ u2/d02/ajax_lesson/script.js
@@ -18,14 +18,14 @@ $.ajax({
 });
 
 function randomData(data){
-  console.log(data.results[0])
+  console.log(data.results[0].name)
   var $imgTag = $('img')
     $imgTag.attr("src", data.results[0].picture.medium)
 
-
+    var $userTitle = $('p#user_title')
     var $nameLi = $('li').eq(0)
     $nameLi.on("mouseover", function(){
-      $nameLi.text('hi')
+      $userTitle.text("Hi, my name is " + data.results[0].name.first)
       console.log('hovered')
     })
 
