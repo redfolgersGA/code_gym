953518cf095d7c4c856e6cf6db0b5caff487c3b5
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-04-30 01:15:54 -0400

    remodeling portfolio

--- PORTFOLIO/scripts/script.js
+++ PORTFOLIO/scripts/script.js
@@ -7,31 +7,31 @@ $($img2).hide()
 
 // return an array of HTML elements....The elements in the array will be associated
 // with an image
-function switchImage() {
-  var $img1 = $('div#img1')
-  var $img2 = $('div#img2')
-  $($img2).hide()
-  $($img1).hide()
-  $($img2).show()
-  $(img1).attr('src', 'img/Headshot.jpg')
-}
+// function switchImage() {
+//   var $img1 = $('div#img1')
+//   var $img2 = $('div#img2')
+//   $($img2).hide()
+//   $($img1).hide()
+//   $($img2).show()
+//   $(img1).attr('src', 'img/Headshot.jpg')
+// }
 
 
-setInterval(switchImage, 2000)
+// setInterval(switchImage, 2000)
 
 
 
-function navBarHide() {
-  var $navBar = $('nav#header_lis')
-  var $h1 = $('div#name h1')
-  $h1.on('click', function(){
-    $navBar.toggle()
+// function navBarHide() {
+//   var $navBar = $('nav#header_lis')
+//   var $h1 = $('div#name h1')
+//   $h1.on('click', function(){
+//     $navBar.toggle()
 
-  })
+//   })
 
 
-}
-navBarHide()
+// }
+// navBarHide()
 
 
 
