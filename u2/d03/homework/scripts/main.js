4daf80e39d20f97d29e4e5c9538a1d47691b061e
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-05-01 13:01:30 -0400

    ajax request upon submit

--- u2/d03/homework/scripts/main.js
+++ u2/d03/homework/scripts/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 console.log("script unloaded sike")
 
+// when you press submit the form triggers an ajax request
 var $inputBox = $('input#city_entered')
+
+$inputBox.on("submit", submitCity(event))
+
+
+function submitCity (event){
+  event.preventDefault()
 
 
 
@@ -14,6 +21,8 @@ $.ajax({
 
 
 })
+
+}
 
 
 
