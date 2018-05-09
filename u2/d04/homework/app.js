7251c38658f5d8eba62e829b58c0b7056d933721
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-05-08 23:51:16 -0400

    html

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 
 var getHTML = function(res){
   res.writeHead(200,{"Content-type": "text/html"});
-  var html = fs.readFileSync(__dirname + "/public/index.html");
+  var html = fs.readFileSync(__dirname + "");
   res.end(html);
 
 }
