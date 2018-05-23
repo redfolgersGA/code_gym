36f4829c605ff5dd77c916a02dedb82a5e8eb305
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-05-23 14:06:25 -0400

    render HTML

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -3,35 +3,14 @@
 var http = require("http");
 var fs = require("fs");
 
+var port = 8080;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var getHTML = function(res){
+  res.writeHead(200, {"Content-type": "text/html"});
+  var html = fs.readFileSync(__dirname + "/public/index.html");
+  res.end(html);
+}
 
 // var http = require("http");
 // var fs = require("fs");
