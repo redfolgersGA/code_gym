36610f728ddf58c31524a9b02f8058232f69e9de
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-05-23 14:15:43 -0400

    server listening

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -12,6 +12,20 @@ var getHTML = function(res){
   res.end(html);
 }
 
+var getCSS = function(res){
+  res.writeHead(200,{"Content-type": "text/css"});
+  var css = fs.readFileSync(__dirname + "/public/style.css");
+  res.end(css);
+}
+
+var server = http.createServer(function(res, req){
+      if(req.url === "/style.css") {
+        getCSS(res);
+      } else if (req.url === "/") {
+        getHTML(res)
+
+      }
+})
 // var http = require("http");
 // var fs = require("fs");
 
