3e9a2a914ab980ee0448056d9c115e620d13ffd7
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-06-16 13:26:50 -0400

    res object sends back html and css files

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -1,2 +1,33 @@
 var http = require("http");
 var fs = require("fs");
+
+var getHTML = function(res){
+  res.writeHead(200, {"Content-type": "text/html"});
+  var html = fs.readFileSync(__dirname + "/public/index.html");
+  res.end(html);
+}
+
+var getCSS = function(res){
+  res.writeHead(200, {"Content-type": "text/css"});
+  var css = fs.readFileSync(__dirname + "/public/style.css");
+  res.end(css);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var port = 8080;
