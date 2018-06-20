8d760fbd8f2d1ae8a7112c1194d038695059920e
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-06-20 11:06:01 -0400

    cleared

--- u2/d06/morning/script.js
+++ u2/d06/morning/script.js
@@ -1,39 +1,51 @@
 $(document).ready(function(){
-  console.log("loaded");
-  var $body = $('body');
-
-  $('button').click(function getResults(){
-  $('body').removeClass('item');
+console.log("loaded")
 
 
-  })
+
+
+
+
+
+
+
+  // END END END END
+})
+  // console.log("loaded");
+  // var $body = $('body');
+
+  // $('button').click(function(){
+  // $('body').removeClass('item');
+
+
+  // })
 
   // getResults function goes here
-  function getResults(){
-    $('body').removeClass('item');
-  }
+  // function getResults(){
+  //   $('body').removeClass('item');
+  // }
 
   // Event Handlers go here
 
-$.ajax({
-url: 'https://itunes.apple.com/search',
-method: 'GET',
-data : { term: query, media: "music",entity: "album",limit: 25}
-success: function(data) {
-  result = data.results;
-  $results = $('<ul>');
-  var $item, $thumbnail, $description, $artist, $album, $price;
-  ul.forEach()
-}
-})
+// $.ajax({
+// url: 'https://itunes.apple.com/search',
+// method: 'GET',
+// data : { term: query, media: "music",entity: "album",limit: 25}
+// success: function(data) {
+//   result = data.results;
+//   $results = $('<ul>');
+//   var $item, $thumbnail, $description, $artist, $album, $price;
+//   ul.forEach()
+// }
+// })
 
 
-});
+// });
 
-$.ajax({
-  url:
-  type:
-  data:
-  success:
-  error:
-})
+// $.ajax({
+//   url:
+//   type:
+//   data:
+//   success:
+//   error:
+// })
