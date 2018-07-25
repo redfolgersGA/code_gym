ca9b5c585ee764f69d3e90b026767b385886b40b
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2018-07-25 11:31:47 -0400

    jquery redo

--- u1/d09/homework/scripts/fellowship.js
+++ u1/d09/homework/scripts/fellowship.js
@@ -30,9 +30,65 @@ var keepItSecretKeepItSafe = function() {
   createDiv.addClass("magic-imbued-jewelry");
   createDiv.click(nazgulScreech)
   $('li.hobbit').first().append(createDiv)
+  setTimeout(makeBuddies, 2000)
 }
 
+var makeBuddies = function(){
+  var createAside = $('<aside></aside>');
+  var createUl = $('<ul id="buddies"></ul>')
+  createAside.append(createUl)
+  buddies.forEach(function(i){
+    createUl.append('<li class="buddy">' + i + '</li>')
+  })
 
+  $('article.land').eq(1).append(createUl)
+  setTimeout(beautifulStranger, 2000)
+
+}
+
+var beautifulStranger = function() {
+  var striderBuddy = $('li.buddy').eq(3)
+  striderBuddy.text('Aragon')
+  striderBuddy.css("color", "green")
+  setTimeout(leaveTheShire, 2000)
+}
+
+var leaveTheShire = function() {
+  var assembledHobbits = $('li.hobbit');
+  var rivendell = $('article.land').eq(1)
+
+  rivendell.append(assembledHobbits)
+  setTimeout(forgeTheFellowShip, 2000)
+
+}
+
+var forgeTheFellowShip = function(){
+  var fellowShipDiv = $('<div id="the-fellowship"></div>')
+  var rivendell = $('article.land').eq(1)
+  var hobbitsSelected = $('li.hobbit');
+  var buddiesSelected = $('li.buddy');
+
+  rivendell.append(fellowShipDiv)
+
+  hobbitsSelected.each(function(){
+    $(this).appendTo(fellowShipDiv)
+    alert($(this).text() + ' ' + 'has joined the party')
+
+  })
+
+  buddiesSelected.each(function(){
+    $(this).appendTo(fellowShipDiv)
+    alert($(this).text() + ' ' + 'has joined the party')
+  })
+
+  setTimeout(theBalrog, 2000)
+
+}
+
+var theBalrog = function(){
+  var gandalf = $('li.buddy').eq(0)
+  gandalf.text("Gandalf the White")
+}
 
 
 
