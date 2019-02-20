d65433cfcbccd1a27a00cb5a9816854f6bad44f5
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-02-20 13:10:02 -0500

    change border colors pt 2

--- u1/d06/classwork/moes/access.js
+++ u1/d06/classwork/moes/access.js
@@ -17,3 +17,19 @@ demoId.style.border = '1px solid purple'
 for (i=0; i <demoClass.length; i++) {
   demoClass[i].style.border = '1px solid orange'
 }
+
+// changer border of  demo tag to blue
+
+for (i = 0; i <demoTag.length; i++) {
+  demoTag[i].style.border = '1px solid blue';
+
+}
+
+// Change border of ID demo-query to red
+
+demoQuery.style.border = '1px solid red';
+
+// Change border of class query-all to green
+demoQueryAll.forEach(query => {
+  query.style.border = '1px solid green'
+});
