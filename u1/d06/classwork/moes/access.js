d319e19b3229e56699717331078b9a8508c68f72
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-02-20 13:01:47 -0500

    change border colors

--- u1/d06/classwork/moes/access.js
+++ u1/d06/classwork/moes/access.js
@@ -7,3 +7,13 @@ const demoClass = document.getElementsByClassName('demo');
 const demoTag = document.getElementsByTagName('article');
 const demoQuery = document.querySelector('#demo-query');
 const demoQueryAll = document.querySelectorAll('.demo-query-all');
+
+// change border of ID demo to purple
+
+demoId.style.border = '1px solid purple'
+
+// change border of class demo to orange
+
+for (i=0; i <demoClass.length; i++) {
+  demoClass[i].style.border = '1px solid orange'
+}
