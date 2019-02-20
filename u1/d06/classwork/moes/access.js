c10b35bb511e3b843dde241768dcf493f57c82f0
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-02-20 12:57:56 -0500

    assign all elements

--- u1/d06/classwork/moes/access.js
+++ u1/d06/classwork/moes/access.js
@@ -1,1 +1,9 @@
 console.log('Js linked')
+
+// Step 1 assign all elements
+
+const demoId = document.getElementById('demo');
+const demoClass = document.getElementsByClassName('demo');
+const demoTag = document.getElementsByTagName('article');
+const demoQuery = document.querySelector('#demo-query');
+const demoQueryAll = document.querySelectorAll('.demo-query-all');
