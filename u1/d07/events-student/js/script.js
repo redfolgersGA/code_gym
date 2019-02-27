8cc106bbd54d08e8190b26ff0abc9a751e3fa7f4
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-02-27 10:34:45 -0500

    event listeners

--- u1/d07/events-student/js/script.js
+++ u1/d07/events-student/js/script.js
@@ -1,10 +1,9 @@
 // Example 1 whats going on inside the callback function? displaying the
 // value of i then increasing i + one(++)
+console.log('file is linked')
 var i = 1;
 
-document
-.getElementById('click-button')
-.addEventListener('click', function(event){
+document.getElementById('click-button').addEventListener('click', function(event){
   console.log('i am being clicked.');
     var counter = document.getElementById('click-display');
     counter.innerHTML = 'click count: ' + event.detail;
@@ -12,16 +11,12 @@ document
 
 
 // Example 2
-document
-  .getElementById('hover-button')
-  .addEventListener('mouseenter', function(e){
+document.getElementById('hover-button').addEventListener('mouseenter', function(e){
     var container = document.getElementById('hover-display');
     container.innerHTML = "status: on"
   });
 
-  document
-  .getElementById('hover-button')
-  .addEventListener('mouseleave', function(e){
+  document.getElementById('hover-button').addEventListener('mouseleave', function(e){
     var container = document.getElementById('hover-display');
     container.innerHTML = "status: off"
   });
@@ -37,10 +32,10 @@ input.addEventListener('keyup', function(e){
   container.innerHTML = input.value;
 });
 
-/*
-document
-  .getElementById('input-box')
-  .addEventListener('keypress', function(event){
-    var inputBox
-  })
+
+// document
+//   .getElementById('input-box')
+//   .addEventListener('keypress', function(event){
+//     var inputBox
+//   })
 
