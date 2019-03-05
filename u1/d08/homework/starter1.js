8604e7a551d1285725e786bb25521545e653b94f
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-03-05 10:20:25 -0500

    onmouseover onmouseout

--- u1/d08/homework/starter1.js
+++ u1/d08/homework/starter1.js
@@ -1,71 +1,119 @@
 
 
-console.log('u01d09 hw starter.js linked');
+console.log('u01d09 hw starter.js linked1');
+
+var allBoxes = document.querySelectorAll('.zone')
+
+function changeToGreenAndBack () {
+  for(i=0;i<allBoxes.length;i++){
+
+    allBoxes[i].addEventListener("mouseover", function(e) {
+      this.style.backgroundColor = "green";
 
 
-var boxes = document.querySelectorAll('.zone');
+    })
 
-console.log(boxes)
-
-
-
-function boxesToGreen() {
-  for(i=0;i<boxes.length;i++){
-    boxes[i].onmouseover = styleGreen
-    }
-
-
-
-
-  }
-
-boxesToGreen()
-
-function styleGreen(){
-  this.style.backgroundColor = "green"
-  console.log('style green')
-}
-
-function boxesBackToWhite(){
-  for(i=0;i<boxes.length;i++){
-    boxes[i].onmouseout = styleWhite
-  }
-}
-
-boxesBackToWhite()
-
-function styleWhite() {
+    allBoxes[i].addEventListener('mouseout', function(e){
       this.style.backgroundColor = "white"
-      console.log('style white')
-    }
+    })
 
-
-
-
-function removeWhiteMouse(){
-  for(i=0;i<boxes.length;i++){
-  boxes[i].onclick = clicked
-}
-
-}
-
-removeWhiteMouse()
-
-function clicked(){
-
-  this.style.backgroundColor = "green"
-  this.onmouseout=function(){};
-
-
-
-}
-
-function boxesToGreenPerm(){
-  for(i=0;i<boxes.length;i++){
-    boxes[i].removeEventListener('onmouseover', styleGreen)
-    console.log("greenperm")
   }
+
 }
+
+changeToGreenAndBack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var boxes = document.querySelectorAll('.zone');
+
+// console.log(boxes)
+
+
+
+// function boxesToGreen() {
+//   for(i=0;i<boxes.length;i++){
+//     boxes[i].onmouseover = styleGreen
+//     }
+
+
+
+
+//   }
+
+// boxesToGreen()
+
+// function styleGreen(){
+//   this.style.backgroundColor = "green"
+//   console.log('style green')
+// }
+
+// function boxesBackToWhite(){
+//   for(i=0;i<boxes.length;i++){
+//     boxes[i].onmouseout = styleWhite
+//   }
+// }
+
+// boxesBackToWhite()
+
+// function styleWhite() {
+//       this.style.backgroundColor = "white"
+//       console.log('style white')
+//     }
+
+
+
+
+// function removeWhiteMouse(){
+//   for(i=0;i<boxes.length;i++){
+//   boxes[i].onclick = clicked
+// }
+
+// }
+
+// removeWhiteMouse()
+
+// function clicked(){
+
+//   this.style.backgroundColor = "green"
+//   this.onmouseout=function(){};
+
+
+
+// }
+
+// function boxesToGreenPerm(){
+//   for(i=0;i<boxes.length;i++){
+//     boxes[i].removeEventListener('onmouseover', styleGreen)
+//     console.log("greenperm")
+//   }
+// }
 
 
 
