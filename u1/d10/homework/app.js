cd739cc3c87eb80bf523e0ac0f077bd5ab0f89d8
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-03-18 15:29:21 -0400

    add express to app

--- u1/d10/homework/app.js
+++ u1/d10/homework/app.js
@@ -29,50 +29,110 @@
 // }
 
 // callFunction(sayBye)
-var stuff = require('./stuff')
-var events = require('events')
-var util = require('util')
-var fs = require('fs')
+// var stuff = require('./stuff')
+// var events = require('events')
+// var util = require('util')
+// var fs = require('fs')
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']))
-console.log(stuff.adder(5,6))
-console.log(stuff.adder(stuff.pi, 5))
+// console.log(stuff.counter(['shaun', 'crystal', 'ryu']))
+// console.log(stuff.adder(5,6))
+// console.log(stuff.adder(stuff.pi, 5))
 
-// var myEmitter = new events.EventEmitter();
-// console.log(myEmitter)
+// // var myEmitter = new events.EventEmitter();
+// // console.log(myEmitter)
 
-// myEmitter.on('someEvent', function(mssg){
-//   console.log(mssg)
+// // myEmitter.on('someEvent', function(mssg){
+// //   console.log(mssg)
+
+// // })
+
+// // myEmitter.emit('someEvent', 'the event was emitted')
+
+// var Person = function(name, lastName){
+//   this.name = name;
+//   this.lastName = lastName
+// }
+
+// util.inherits(Person, events.EventEmitter);
+
+// var emmanuel = new Person('emmanuel', 40)
+// var james = new Person('james')
+// var mary = new Person('mary')
+
+// var people = [james, mary, emmanuel]
+
+// people.forEach(function(person){
+//   person.on('speak',function(mssg){
+//     console.log(person.name + ' said ' + mssg)
+//   })
+// })
+
+// james.emit('speak', 'hey dudes')
+// emmanuel.emit('speak', 'I want to run')
+
+// fs.readFile('readme.md', 'utf8', function(err, data){
+//   console.log(data)
+
+// })
+// // fs.writeFileSync('writeMe.txt', readMe)
+// console.log('test')
+// var http = require('http')
+
+
+// var fs = require('fs')
+
+
+// myReadStream.on('data', function(chunk){
+//   console.log('new chunk received:');
+//   myWriteStream.write(chunk)
+
+//   })
+
+
+
+// var server = http.createServer(function(req, res){
+//   console.log('request was made' + req.url)
+//   if(req.url === '/home' || req.url === '/'){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/index.html').pipe(res)
+//   } else if(req.url === '/contact'){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/contact.html').pipe(res)
+
+//   } else if(req.url === '/api/ninjas'){
+//     var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age:32}];
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify(ninjas))
+//   } else {
+//     res.writeHead(404, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/404.html').pipe(res)
+
+//   }
+//   // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+
+//   // myReadStream.pipe(res)
 
 // })
 
-// myEmitter.emit('someEvent', 'the event was emitted')
+// server.listen(3000, '127.0.0.1')
+// console.log("yo dawgs, now listeining to port 3000")
+// returns the express module(object) via modules.export
+var express = require('express');
+// fires the express function inside that function there are props and methods accessible to us
+var app = express()
 
-var Person = function(name, lastName){
-  this.name = name;
-  this.lastName = lastName
-}
+app.get('/', function(req, res){
+  res.send('This is the homepage')
+});
 
-util.inherits(Person, events.EventEmitter);
-
-var emmanuel = new Person('emmanuel', 40)
-var james = new Person('james')
-var mary = new Person('mary')
-
-var people = [james, mary, emmanuel]
-
-people.forEach(function(person){
-  person.on('speak',function(mssg){
-    console.log(person.name + ' said ' + mssg)
-  })
+app.get('/contact', function(req, res){
+  res.send('This is the contact page')
 })
 
-james.emit('speak', 'hey dudes')
-emmanuel.emit('speak', 'I want to run')
+app.listen(3000)
 
-var readMe = fs.readFileSync('readme.md', 'utf8')
 
-console.log(readMe)
+
 
 
 
