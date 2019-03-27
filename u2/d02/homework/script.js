935e8127cfb064223e9c91eda4df1fd156d857c2
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-03-27 15:24:04 -0400

    htting the database

--- u2/d02/homework/script.js
+++ u2/d02/homework/script.js
@@ -1,18 +1,49 @@
 $(document).ready(function(){
 
-  var getData = function(movieTitle){
 
-     $.ajax({
-        url: "http://www.omdbapi.com/?t=" + movieTitle + "&r=json",
+
+var input = $('input.u-full-width')
+var movieTitle = input.val()
+
+var apiKeyAccess = "http://www.omdbapi.com/?t=" + movieTitle +
+
+
+
+var getData = function(movieTitle){
+  var button = $('button.u-full-width')
+  button.on('click', function(){
+    $.ajax({
+        url: apiKeyAccess,
         method: "get",
         success: function(data){
+
+
            console.log(data);
-          handleResponse(data);
+
+           var input = $('input.u-full-width')
+           var movieNameH5Tag = $('h5');
+
+
+
+           movieNameH5Tag.text(data.Title)
+          // handleResponse(data);
         }
       })
-  };
 
-  getData();
+  })
+}
+
+getData()
+
+
+  // "http://www.omdbapi.com/?t=" + movieTitle + "&r=json",
+
+  // var getData = function(movieTitle){
+
+
+  // // };
+
+  // getData("kindergarten cop 2");
 
 
 
@@ -21,28 +52,28 @@ $(document).ready(function(){
 
 //every input tag has a hidden "value=" attribute that by default is
 //an empty string " " . .val() takes what the user writes in the input and holds it
-  var addAJAXFunction = function(){
-    $('button').on('click', function(){
-    var inputValue = $('input').val();
-    console.log(inputValue);
-    getData(inputValue);
+  // var addAJAXFunction = function(){
+  //   $('button').on('click', function(){
+  //   var inputValue = $('input').val();
+  //   console.log(inputValue);
+  //   getData(inputValue);
 
 
-    })
-  };
+  //   })
+  // };
 
-  var appendMovie = function(name,imagePath){
-    $('h5').text(name);
-    $('img').attr('src', imagePath);
-  };
+  // var appendMovie = function(name,imagePath){
+  //   $('h5').text(name);
+  //   $('img').attr('src', imagePath);
+  // };
 
-  var handleResponse = function(data){
-    var movieName = data.Title
-    var movieImage = data.Poster
-    console.log(movieName);
-    console.log(movieImage);
-    appendMovie(movieName, movieImage)
+  // var handleResponse = function(data){
+  //   var movieName = data.Title
+  //   var movieImage = data.Poster
+  //   console.log(movieName);
+  //   console.log(movieImage);
+  //   appendMovie(movieName, movieImage)
 
-  };
+  // };
 
 }); // ends doc.ready
