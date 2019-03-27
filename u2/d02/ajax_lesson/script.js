85672c17a72ea33ba39427714d18232fd14c22b9
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-03-27 12:04:54 -0400

    adds user image user name and user email to DOM

--- u2/d02/ajax_lesson/script.js
+++ u2/d02/ajax_lesson/script.js
@@ -39,9 +39,30 @@ $.ajax({
   type: "GET",
   url: "https://randomuser.me/api/",
   success: function(data){
-    console.log(data.results[0].picture.medium)
+    console.log(data.results[0])
     var img = $('img')
     img.attr('src', data.results[0].picture.medium)
+    var titleParagraph = $('#user_title')
+    var userValue = $('#user_value')
+    userValue.text(data.results[0].name.first + " " + data.results[0].name.last)
+    var nameLi = $('li').eq(0)
+    nameLi.on('mouseover', function(){
+      userValue.text(data.results[0].name.first + " " + data.results[0].name.last)
+      titleParagraph.text('Hello, My name is ')
+
+
+    })
+    var emailLi = $('li').eq(1)
+      emailLi.on('mouseover', function(){
+        var titleParagraph = $('#user_title')
+        titleParagraph.text('My email is ')
+        userValue.text(data.results[0].email)
+
+      })
+
+
+
+
   }
 })
 
