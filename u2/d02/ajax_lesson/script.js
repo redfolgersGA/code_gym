a9b72e888a550d378a5b7cf710276b05065592a8
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-03-27 11:34:33 -0400

    retreiving data

--- u2/d02/ajax_lesson/script.js
+++ u2/d02/ajax_lesson/script.js
@@ -10,32 +10,40 @@ console.log('jsloaded')
 
 // turnPTagsRed()
 
+// $.ajax({
+//   url: 'https://randomuser.me/api/',
+//   dataType: 'json',
+//   success: randomData
+
+// });
+
+// function randomData(data){
+//   console.log(data.results[0].name)
+//   var $imgTag = $('img')
+//     $imgTag.attr("src", data.results[0].picture.medium)
+
+//     var $userTitle = $('p#user_title')
+//     var $nameLi = $('li').eq(0)
+//     $nameLi.on("mouseover", function(){
+//       $userTitle.text("Hi, my name is " + data.results[0].name.first)
+//       console.log('hovered')
+//     })
+
+
+// }
+
+
+
+
 $.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: randomData
-
-});
-
-function randomData(data){
-  console.log(data.results[0].name)
-  var $imgTag = $('img')
-    $imgTag.attr("src", data.results[0].picture.medium)
-
-    var $userTitle = $('p#user_title')
-    var $nameLi = $('li').eq(0)
-    $nameLi.on("mouseover", function(){
-      $userTitle.text("Hi, my name is " + data.results[0].name.first)
-      console.log('hovered')
-    })
-
-
-}
-
-
-
-
-
+  type: "GET",
+  url: "https://randomuser.me/api/",
+  success: function(data){
+    console.log(data.results[0].picture.medium)
+    var img = $('img')
+    img.attr('src', data.results[0].picture.medium)
+  }
+})
 
 
 
