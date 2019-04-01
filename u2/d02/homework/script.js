b4d17b6177126115c320b9c1c0b0b33d36504e85
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-04-01 01:16:02 -0400

    adding

--- u2/d02/homework/script.js
+++ u2/d02/homework/script.js
@@ -2,48 +2,45 @@ $(document).ready(function(){
 
 
 
-var input = $('input.u-full-width')
-var movieTitle = input.val()
 
-var apiKeyAccess = "http://www.omdbapi.com/?t=" + movieTitle +
-
-
-
-var getData = function(movieTitle){
-  var button = $('button.u-full-width')
-  button.on('click', function(){
-    $.ajax({
-        url: apiKeyAccess,
-        method: "get",
-        success: function(data){
+var movieTitle = "babe"
+// var input = $('input.u-full-width')
+// var movieTitle = input.val()
 
 
-           console.log(data);
+// var apiKey = "http://www.omdbapi.com/?t="+movieTitle+"apikey"
 
-           var input = $('input.u-full-width')
-           var movieNameH5Tag = $('h5');
-
-
-
-           movieNameH5Tag.text(data.Title)
-          // handleResponse(data);
-        }
-      })
-
-  })
-}
-
-getData()
-
-
-  // "http://www.omdbapi.com/?t=" + movieTitle + "&r=json",
-
-  // var getData = function(movieTitle){
+// var button = $('button.u-full-width')
+// button.on('click', function(){
+//   $.ajax({
+//     url: apiKey,
+//     method: "GET",
+//     success: function(data){
+//       console.log(data)
+//       var h5 = $('h5')
+//       var img = $('img')
+//       h5.text(data.Title)
+//       img.attr('src', data.Poster)
+//     }
+//   })
+// })
 
 
-  // // };
 
-  // getData("kindergarten cop 2");
+
+
+
+
+
+
+  var getData = function(movieTitle){
+    var h5 = $('h5')
+    h5.text(movieTitle)
+
+
+  };
+
+  getData();
 
 
 
@@ -52,15 +49,17 @@ getData()
 
 //every input tag has a hidden "value=" attribute that by default is
 //an empty string " " . .val() takes what the user writes in the input and holds it
-  // var addAJAXFunction = function(){
-  //   $('button').on('click', function(){
-  //   var inputValue = $('input').val();
-  //   console.log(inputValue);
-  //   getData(inputValue);
+  var addAJAXFunction = function(){
+    $('button').on('click', function(){
+    var inputValue = $('input').val();
+    console.log(inputValue);
+    getData(inputValue);
 
 
-  //   })
-  // };
+    })
+  };
+
+  addAJAXFunction()
 
   // var appendMovie = function(name,imagePath){
   //   $('h5').text(name);
