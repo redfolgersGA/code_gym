39a9255274e38169c337637ea5657048071447e7
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-04-04 02:01:22 -0400

    render html

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -7,6 +7,12 @@ var fs = require("fs");
 //   res.end(html);
 // }
 
+var getHTML = function(res){
+  res.writeHead(200, {"Content-type": "text/html"});
+  var html = fs.readFileSync(__dirname + "/public/index.html");
+  res.end(html)
+}
+
 var getCSS = function(res){
   res.writeHead(200, {"Content-type": "text/css"});
   var css = fs.readFileSync(__dirname + "/public/style.css");
