cc217ff00602ef1d9c213bccee00359f10bb16f1
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-04-04 01:58:39 -0400

    server test

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -1,11 +1,11 @@
 var http = require("http");
 var fs = require("fs");
 
-var getHTML = function(res){
-  res.writeHead(200, {"Content-type": "text/html"});
-  var html = fs.readFileSync(__dirname + "/public/index.html");
-  res.end(html);
-}
+// var getHTML = function(res){
+//   res.writeHead(200, {"Content-type": "text/html"});
+//   var html = fs.readFileSync(__dirname + "/public/index.html");
+//   res.end(html);
+// }
 
 var getCSS = function(res){
   res.writeHead(200, {"Content-type": "text/css"});
