c0a44bebd16464c23f334581c9a3b8876c107579
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-04-10 04:54:52 -0400

    array obj key vals

--- u2/d05/morning/hamlet.js
+++ u2/d05/morning/hamlet.js
@@ -62,6 +62,18 @@ console.log(finalArrayOfArrays)
 
 
 
+var countedNames = finalArrayOfArrays[0].reduce(function (allNames, name) {
+
+  if (name in allNames) {
+    allNames[name]++;
+  }
+  else {
+    allNames[name] = 1;
+  }
+  return allNames;
+}, {});
+
+
 
 
 
