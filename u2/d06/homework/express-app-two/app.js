0a90d7e9eac4ffb9505756d5ec22bab529155e31
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-04-24 11:58:08 -0400

    serving index file when navigation to /

---
+++ u2/d06/homework/express-app-two/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.listen(port)
+
+console.log('Server started on ' + port);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('index')
+})
