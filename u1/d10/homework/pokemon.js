d6d0b3c699f331ffdeb9027587a652e8d8e8c21b
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-11-18 00:04:11 -0500

    fixing attack button

--- u1/d10/homework/pokemon.js
+++ u1/d10/homework/pokemon.js
@@ -62,18 +62,23 @@ $("document").ready(function(){
 
 
   var attackButton = function() {
+    var enemyHealthBar = $('p.health').eq(1)
     var enemyHealthBarInteger = parseInt(enemyHealthBar.text())
     console.log(enemyHealthBarInteger)
     var attackButton = $('p#attack_btn')
 
     attackButton.click(function(){
       console.log('attackButton clicked')
+      var randomAttackNumber = Math.floor(Math.random() * (30 - 5 + 1)) + 5
+      var randomAttacked = enemyHealthBarInteger - randomAttackNumber
+      enemyHealthBar.text(randomAttackNumber)
 
     })
 
   }
 
   attackButton()
+
 
 
 
