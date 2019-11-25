502595455dae3ab3a6041b0d268206d60617c34a
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-11-25 03:57:26 -0500

    all user data logged onto DOM

--- u2/d02/ajax_lesson/script.js
+++ u2/d02/ajax_lesson/script.js
@@ -32,8 +32,8 @@ console.log('jsloaded')
 
 // }
 
-var userName = $("p#user_value")
-// var data = "data.results"
+var userValue = $("p#user_value")
+var userTitle = $('p#user_title')
 var userPhoto = $('img')
 var liTags = $('li')
 var nameLi = liTags.eq(0)
@@ -48,12 +48,29 @@ $.ajax({
   url: "https://randomuser.me/api/",
   success: function(data) {
     var shortData = data.results[0]
-    userName.text(shortData.name.first + " " + shortData.name.last)
+    userValue.text(shortData.name.first + " " + shortData.name.last)
 
 
     userPhoto.attr('src', shortData.picture.large)
     nameLi.click(function(){
-      userName.text(shortData.name.first + " " + shortData.name.last)
+      userTitle.text("Hello, My name is")
+      userValue.text(shortData.name.first + " " + shortData.name.last)
+    })
+    emailLi.click(function(){
+      userTitle.text("My email is:")
+      userValue.text(shortData.email)
+    })
+    bDayLi.click(function(){
+      userTitle.text("My birthday is")
+      userValue.text(shortData.dob.date)
+    })
+    addressLi.click(function(){
+      userTitle.text("My city is")
+      userValue.text(shortData.location.city)
+    })
+    passwordLi.click(function(){
+      userTitle.text("My password is:")
+      userValue.text(shortData.login.password)
     })
   }
 
