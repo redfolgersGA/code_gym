bb47c706076cf51d8d805e2ed006cb78a664a2ff
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-12-06 04:53:10 -0500

    extreme makeover navbar edition

--- Fitness:Health Website/testimonials/testi.js
+++ Fitness:Health Website/testimonials/testi.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
 console.log("loaded")
 
+var navSlide= function(){
+  var burger = document.querySelector('.burger')
+  var nav = document.querySelector('.nav-links')
+
+  burger.addEventListener('click',function(){
+    nav.classList.toggle('nav-active')
+  })
+}
+
+var app = function(){
+  navSlide()
 
 
+}
 
+app()
 
 
 
