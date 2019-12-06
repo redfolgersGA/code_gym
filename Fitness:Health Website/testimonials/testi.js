cf414509b71c1bc88f7c6a52cd0a45b7adca533c
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-12-06 05:32:51 -0500

    responsive navbar

--- Fitness:Health Website/testimonials/testi.js
+++ Fitness:Health Website/testimonials/testi.js
@@ -2,12 +2,31 @@ $(document).ready(function() {
 console.log("loaded")
 
 var navSlide= function(){
-  var burger = document.querySelector('.burger')
-  var nav = document.querySelector('.nav-links')
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('.nav-links');
+  var navLinks = document.querySelectorAll('.nav-links li');
 
+  // Toggle Nav
   burger.addEventListener('click',function(){
     nav.classList.toggle('nav-active')
-  })
+
+
+    // Animate Links
+      navLinks.forEach(function(link, index){
+        if(link.style.animation){
+          link.style.animation = ''
+        }else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.2}s`
+
+
+      }
+    });
+    // Burger Animation
+    burger.classList.toggle('toggle')
+
+  });
+
+
 }
 
 var app = function(){
