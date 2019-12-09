8bc2a4f60efada1fc59bc95b6d1e58a1fff7bb77
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-12-09 06:09:50 -0500

    its not xmas but it will be in 16 days

--- StoreProject/script.js
+++ StoreProject/script.js
@@ -6,7 +6,7 @@ var banner = $('.banner')
 var xmasMonth = 11;
 var xmasDay = 25
 var daysTillXmas = xmasDay - d.getUTCDate()
-console.log(daysTillXmas)
+
 
 
 
@@ -14,10 +14,11 @@ console.log(d.toDateString())
 
 console.log(d.getUTCDate())
 
-if(d.getUTCDate() === xmasDay){
+if(d.getUTCDate() === xmasDay && d.getUTCMonth() === xmasMonth){
   console.log("its christmas!")
 } else{
-  console.log('its not christmas')
+  console.log('its not christmas but it will be in ' + daysTillXmas + ' days')
+
 }
 
 
