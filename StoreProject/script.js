40cf808e51830a13e6be1d15b2509ecc4c3a6016
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2019-12-09 06:05:22 -0500

    checking to see if its xmas

--- StoreProject/script.js
+++ StoreProject/script.js
@@ -1,3 +1,31 @@
+$(document).ready(function(){
 console.log("loaded")
+  // MAKE .banner into a countdown for christmas
+var banner = $('.banner')
+var xmasMonth = 11;
+var xmasDay = 25
 
-// MAKE .banner into a countdown for christmas
+var d = new Date();
+
+console.log(d.toDateString())
+
+console.log(d.getUTCDate())
+
+if(d.getUTCDate() === xmasDay){
+  console.log("its christmas!")
+} else{
+  console.log('its not christmas')
+}
+
+
+
+
+
+
+
+
+
+
+})
+
+
