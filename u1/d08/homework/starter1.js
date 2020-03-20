81bdb258ac124225de6c46af5907ea9942612f31
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-03-20 00:39:48 -0400

    onmouseover/out vs addeventlistener

--- u1/d08/homework/starter1.js
+++ u1/d08/homework/starter1.js
@@ -2,102 +2,147 @@
 
 console.log('u01d09 hw starter.js linked1');
 
-var allZones = document.querySelectorAll('.zone');
-  var zone1Clicked = false
-  var zone2Clicked = false
-  var zone3Clicked = false
-  var zone4Clicked = false
 
-for (var i = 0; i < allZones.length; i++){
-  allZones[i].onmouseover = function() {
-    this.setAttribute('style', 'background-color: green');
-  };
+var allZones = document.querySelectorAll('.zone')
 
-  allZones[i].onmouseout = function() {
-    this.setAttribute('style', 'background-color: white');
-  };
+for(var i=0;i<allZones.length;i++){
+  allZones[i].onmouseover = function(){
+    this.style.backgroundColor ="green"
 
-  allZones[i].onclick = function() {
-    this.setAttribute('style', 'background-color: red');
-
-    this.onmouseover = function() {
-      //do nothing after clicked
-    };
-
-    this.onmouseout = function() {
-      //do nothing after clicked
-    };
   }
-};
 
+  allZones[i].onmouseout = function(){
+    this.style.backgroundColor = "white"
 
+  }
 
-function checkIfAllZonesRed () {
+  allZones[i].addEventListener('click', function(){
+    this.style.backgroundColor = "green"
 
+    this.onmouseover = function () {
 
-  var zone1 = document.querySelectorAll('.zone')[0]
-  var zone2 = document.querySelectorAll('.zone')[1]
-  var zone3 = document.querySelectorAll('.zone')[2]
-  var zone4 = document.querySelectorAll('.zone')[3]
-
-  zone1.addEventListener('click', function(){
-    if(zone1Clicked == false){
-      zone1.style.backgroundColor = 'red'
-    zone1Clicked = true
-    console.log(zone1Clicked)
-    checkZonesPart2()
-
-
-    } else{ (zone1Clicked == true)
-      zone1.addEventListener('click', function(){
-        zone1.style.backgroundColor = 'purple'
-        zone1Clicked = false
-        console.log(zone1Clicked)
-      })
     }
 
+    this.onmouseout = function () {
+
+    }
   })
-  zone2.addEventListener('click', function(){
-    zone2Clicked = true
-    console.log(zone2Clicked)
-    checkZonesPart2()
-  })
-
-  zone3.addEventListener('click', function(){
-    zone3Clicked = true
-    console.log(zone3Clicked)
-    checkZonesPart2()
-  })
-  zone4.addEventListener('click', function(){
-
-    zone4Clicked = true
-    console.log(zone4Clicked)
-    checkZonesPart2()
-
-  })
-
-
-
-
-
-
 }
-checkIfAllZonesRed()
-
-
-function checkZonesPart2(){
-  if(zone1Clicked == true && zone2Clicked == true && zone3Clicked == true && zone4Clicked == true){
-    alert("YOU WON!")
-  }
-
-
-
-}
-console.log(zone1Clicked)
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------3/20/20
+
+// var allZones = document.querySelectorAll('.zone');
+//   var zone1Clicked = false
+//   var zone2Clicked = false
+//   var zone3Clicked = false
+//   var zone4Clicked = false
+
+// for (var i = 0; i < allZones.length; i++){
+//   allZones[i].onmouseover = function() {
+//     this.setAttribute('style', 'background-color: green');
+//   };
+
+//   allZones[i].onmouseout = function() {
+//     this.setAttribute('style', 'background-color: white');
+//   };
+
+//   allZones[i].onclick = function() {
+//     this.setAttribute('style', 'background-color: red');
+
+//     this.onmouseover = function() {
+//       //do nothing after clicked
+//     };
+
+//     this.onmouseout = function() {
+//       //do nothing after clicked
+//     };
+//   }
+// };
+
+
+
+// function checkIfAllZonesRed () {
+
+
+//   var zone1 = document.querySelectorAll('.zone')[0]
+//   var zone2 = document.querySelectorAll('.zone')[1]
+//   var zone3 = document.querySelectorAll('.zone')[2]
+//   var zone4 = document.querySelectorAll('.zone')[3]
+
+//   zone1.addEventListener('click', function(){
+//     if(zone1Clicked == false){
+//       zone1.style.backgroundColor = 'red'
+//     zone1Clicked = true
+//     console.log(zone1Clicked)
+//     checkZonesPart2()
+
+
+//     } else{ (zone1Clicked == true)
+//       zone1.addEventListener('click', function(){
+//         zone1.style.backgroundColor = 'purple'
+//         zone1Clicked = false
+//         console.log(zone1Clicked)
+//       })
+//     }
+
+//   })
+//   zone2.addEventListener('click', function(){
+//     zone2Clicked = true
+//     console.log(zone2Clicked)
+//     checkZonesPart2()
+//   })
+
+//   zone3.addEventListener('click', function(){
+//     zone3Clicked = true
+//     console.log(zone3Clicked)
+//     checkZonesPart2()
+//   })
+//   zone4.addEventListener('click', function(){
+
+//     zone4Clicked = true
+//     console.log(zone4Clicked)
+//     checkZonesPart2()
+
+//   })
+
+
+
+
+
+
+// }
+// checkIfAllZonesRed()
+
+
+// function checkZonesPart2(){
+//   if(zone1Clicked == true && zone2Clicked == true && zone3Clicked == true && zone4Clicked == true){
+//     alert("YOU WON!")
+//   }
+
+
+
+// }
+// console.log(zone1Clicked)
+
+
+
+// --------------------------------------------------------------------
 
 
 
