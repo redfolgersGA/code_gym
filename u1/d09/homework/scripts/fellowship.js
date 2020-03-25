e955deddea93b1fe0afeccab6bf10386df2549dd
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-03-25 09:48:50 -0400

    reps

--- u1/d09/homework/scripts/fellowship.js
+++ u1/d09/homework/scripts/fellowship.js
@@ -1,207 +1,258 @@
 console.log("fellowship.js loaded code_gym redo");
 
+
 var body = $('body')
 
+var makeMiddleEarth = function (){
+  var sectionTag = $('<section id="middle-earth"></section>')
 
+  for(var i=0;i<lands.length;i++){
+    var articleTag = $('<article>' + lands[i] + '</article>')
+    articleTag.attr('class', 'land')
+    articleTag.html("<h1>" + lands[i] + "</h1>")
 
-var makeMiddleEarth = function(){
-  var middleEarth = $('<section></section>')
-  middleEarth.attr('id', 'middle-earth')
-
-  for(var i = 0; i < lands.length; i++){
-    middleEarth.append('<article class ="land"><h1>' + lands[i] + '</h1></article>')
+    sectionTag.append(articleTag)
 
 
 
   }
-  body.append(middleEarth)
-  setTimeout(makeHobbits, 2000)
+
+  body.append(sectionTag)
+
+
+
+
 
 
 }
-
 
 makeMiddleEarth()
 
-function makeHobbits () {
-  var theShire = $('article.land').eq(0)
-  var hobbitsUl = $('<ul></ul>')
-  hobbitsUl.attr('id', 'hobbits')
-
-  console.log(hobbitsUl)
-
-  for(var i =0; i < hobbits.length; i++){
-    hobbitsUl.append('<li class=hobbit>' + hobbits[i] + '</li>')
-  }
-
-  theShire.append(hobbitsUl)
-
-  setTimeout(keepItSecretKeepItSafe, 2000)
-
-}
 
 
-//   function makeHobbits() {
-//   var theShire = $('article.land').first()
-//   var hobbitUl = $('<ul id=hobbits></ul>')
-//   for(i=0;i<hobbits.length;i++){
-//     hobbitUl.append('<li class="hobbit">' + hobbits[i] + '</li>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3/25/2020-------------------------------------------------------------------------
+
+// var body = $('body')
+
+
+
+// var makeMiddleEarth = function(){
+//   var middleEarth = $('<section></section>')
+//   middleEarth.attr('id', 'middle-earth')
+
+//   for(var i = 0; i < lands.length; i++){
+//     middleEarth.append('<article class ="land"><h1>' + lands[i] + '</h1></article>')
+
+
 
 //   }
-//   theShire.append(hobbitUl)
-//   // setTimeout(keepItSecretKeepItSafe, 2000)
+//   body.append(middleEarth)
+//   setTimeout(makeHobbits, 2000)
+
+
 // }
 
-var keepItSecretKeepItSafe = function() {
-var body = $('body')
-var clickCounter = 0
-var frodo = $('li.hobbit').eq(0)
-var theRingDiv = $('<div id="the-ring"</div>')
-theRingDiv.attr('class', 'magic-imbued-jewelry')
-theRingDiv.click(function(){
-  clickCounter++
-  if(clickCounter >= 3){
 
-    body.text('The world is over')
-  }else {
-  nazgulScreech()
-  frodo.fadeOut(1000, function(){
-  frodo.fadeIn(1000)
-  })
+// makeMiddleEarth()
 
-  }
-  console.log(clickCounter)
+// function makeHobbits () {
+//   var theShire = $('article.land').eq(0)
+//   var hobbitsUl = $('<ul></ul>')
+//   hobbitsUl.attr('id', 'hobbits')
 
-})
+//   console.log(hobbitsUl)
 
-frodo.append(theRingDiv)
+//   for(var i =0; i < hobbits.length; i++){
+//     hobbitsUl.append('<li class=hobbit>' + hobbits[i] + '</li>')
+//   }
 
-setTimeout(makeBuddies, 2000)
-}
+//   theShire.append(hobbitsUl)
 
-var makeBuddies = function(){
-  var rivendell = $('article.land').eq(1)
-  var asideTagForBuddies = $('<aside></aside>')
-  var ulTagForBuddies = $('<ul></ul>')
-  ulTagForBuddies.attr('id', 'buddies')
+//   setTimeout(keepItSecretKeepItSafe, 2000)
 
-  asideTagForBuddies.append(ulTagForBuddies)
-  console.log(asideTagForBuddies)
+// }
 
-  for(var i =0; i<buddies.length;i++){
-    var buddyLi = $('<li></li>')
-    buddyLi.attr('class', 'buddy')
-    buddyLi.text(buddies[i])
 
-    ulTagForBuddies.append(buddyLi)
-  }
-  rivendell.append(ulTagForBuddies)
+// //   function makeHobbits() {
+// //   var theShire = $('article.land').first()
+// //   var hobbitUl = $('<ul id=hobbits></ul>')
+// //   for(i=0;i<hobbits.length;i++){
+// //     hobbitUl.append('<li class="hobbit">' + hobbits[i] + '</li>')
 
-  setTimeout(beautifulStranger, 2000)
-}
+// //   }
+// //   theShire.append(hobbitUl)
+// //   // setTimeout(keepItSecretKeepItSafe, 2000)
+// // }
 
-var beautifulStranger = function(){
-  var green = "green"
-  var strider = $('li.buddy').eq(3)
-  strider.text('Aragorn')
-  strider.css('color', green)
+// var keepItSecretKeepItSafe = function() {
+// var body = $('body')
+// var clickCounter = 0
+// var frodo = $('li.hobbit').eq(0)
+// var theRingDiv = $('<div id="the-ring"</div>')
+// theRingDiv.attr('class', 'magic-imbued-jewelry')
+// theRingDiv.click(function(){
+//   clickCounter++
+//   if(clickCounter >= 3){
 
-  setTimeout(leaveTheShire, 2000)
+//     body.text('The world is over')
+//   }else {
+//   nazgulScreech()
+//   frodo.fadeOut(1000, function(){
+//   frodo.fadeIn(1000)
+//   })
 
-}
+//   }
+//   console.log(clickCounter)
 
-var leaveTheShire = function(){
-  var hobbits = $('li.hobbit')
-  var rivendell = $('article.land').eq(1)
+// })
 
-  for(var i=0; i < hobbits.length; i++){
-    rivendell.append(hobbits[i])
-    console.log(hobbits[i].innerText + " has arrived!")
+// frodo.append(theRingDiv)
 
-  }
+// setTimeout(makeBuddies, 2000)
+// }
 
-  setTimeout(forgeTheFellowship, 2000)
-}
+// var makeBuddies = function(){
+//   var rivendell = $('article.land').eq(1)
+//   var asideTagForBuddies = $('<aside></aside>')
+//   var ulTagForBuddies = $('<ul></ul>')
+//   ulTagForBuddies.attr('id', 'buddies')
 
-var forgeTheFellowship = function() {
-  var theFellowshipDiv = $('<div id=the-fellowship></div>')
-  var rivendell = $('article.land').eq(1)
-  var hobbits = $('li.hobbit')
-  var buddies = $('li.buddy')
+//   asideTagForBuddies.append(ulTagForBuddies)
+//   console.log(asideTagForBuddies)
 
-  rivendell.append(theFellowshipDiv)
+//   for(var i =0; i<buddies.length;i++){
+//     var buddyLi = $('<li></li>')
+//     buddyLi.attr('class', 'buddy')
+//     buddyLi.text(buddies[i])
 
-  for(var i = 0; i < hobbits.length; i++){
-    theFellowshipDiv.append(hobbits[i])
-    alert(hobbits[i].innerText + ' has joined the party')
-  }
+//     ulTagForBuddies.append(buddyLi)
+//   }
+//   rivendell.append(ulTagForBuddies)
 
-  for(var i = 0; i < buddies.length; i++){
-    theFellowshipDiv.append(buddies[i])
-    alert(buddies[i].innerText + ' has joined the party')
-  }
+//   setTimeout(beautifulStranger, 2000)
+// }
 
-  setTimeout(theBalrog, 2000)
-}
+// var beautifulStranger = function(){
+//   var green = "green"
+//   var strider = $('li.buddy').eq(3)
+//   strider.text('Aragorn')
+//   strider.css('color', green)
 
-var theBalrog = function() {
-  var gandalf = $('li.buddy').eq(0)
+//   setTimeout(leaveTheShire, 2000)
 
-  gandalf.text("Gandlaf the White")
-  gandalf.css("border", "3px solid white")
+// }
 
-  setTimeout(hornOfGondor, 2000)
-}
+// var leaveTheShire = function(){
+//   var hobbits = $('li.hobbit')
+//   var rivendell = $('article.land').eq(1)
 
-var hornOfGondor = function(){
-  alert("The Horn of Gondor has been blown")
-  var boromir = $('li.buddy').eq(4)
+//   for(var i=0; i < hobbits.length; i++){
+//     rivendell.append(hobbits[i])
+//     console.log(hobbits[i].innerText + " has arrived!")
 
-  boromir.css("text-decoration", "line-through")
-  boromir.css("opacity", "0.3")
+//   }
 
-  setTimeout(itsDangerousToGoAlone, 2000)
-}
+//   setTimeout(forgeTheFellowship, 2000)
+// }
 
-var itsDangerousToGoAlone = function(){
-  var frodo = $('li.hobbit').eq(0)
-  var mcGee = $('li.hobbit').eq(1)
-  var mordor = $('article.land').eq(2)
+// var forgeTheFellowship = function() {
+//   var theFellowshipDiv = $('<div id=the-fellowship></div>')
+//   var rivendell = $('article.land').eq(1)
+//   var hobbits = $('li.hobbit')
+//   var buddies = $('li.buddy')
 
-  mordor.append(frodo)
-  mordor.append(mcGee)
+//   rivendell.append(theFellowshipDiv)
 
-  var mountDoom = $('<div id=mount-doom></div>')
+//   for(var i = 0; i < hobbits.length; i++){
+//     theFellowshipDiv.append(hobbits[i])
+//     alert(hobbits[i].innerText + ' has joined the party')
+//   }
 
-  mordor.append(mountDoom)
+//   for(var i = 0; i < buddies.length; i++){
+//     theFellowshipDiv.append(buddies[i])
+//     alert(buddies[i].innerText + ' has joined the party')
+//   }
 
-  setTimeout(weWantsIt, 2000)
-}
+//   setTimeout(theBalrog, 2000)
+// }
 
-var weWantsIt = function(){
-  var gollum = $('<div id=gollum></div>')
-  var theRing = $('div#the-ring')
-  var mountDoom = $('div#mount-doom').eq(0)
+// var theBalrog = function() {
+//   var gandalf = $('li.buddy').eq(0)
 
-  gollum.append(theRing)
-  mountDoom.append(gollum)
+//   gandalf.text("Gandlaf the White")
+//   gandalf.css("border", "3px solid white")
 
-  setTimeout(thereAndBackAgain, 2000)
-}
+//   setTimeout(hornOfGondor, 2000)
+// }
 
-var thereAndBackAgain = function(){
-  var gollum = $('div#gollum')
-  var theRing = $('div#the-ring')
-  var hobbits = $('li.hobbit')
-  var theShire = $('article.land').eq(0)
-  var mordor = $('article.land').eq(2)
+// var hornOfGondor = function(){
+//   alert("The Horn of Gondor has been blown")
+//   var boromir = $('li.buddy').eq(4)
 
-  gollum.remove()
+//   boromir.css("text-decoration", "line-through")
+//   boromir.css("opacity", "0.3")
 
-  theShire.append(hobbits)
-  mordor.attr('class', 'collapse')
+//   setTimeout(itsDangerousToGoAlone, 2000)
+// }
 
-}
+// var itsDangerousToGoAlone = function(){
+//   var frodo = $('li.hobbit').eq(0)
+//   var mcGee = $('li.hobbit').eq(1)
+//   var mordor = $('article.land').eq(2)
+
+//   mordor.append(frodo)
+//   mordor.append(mcGee)
+
+//   var mountDoom = $('<div id=mount-doom></div>')
+
+//   mordor.append(mountDoom)
+
+//   setTimeout(weWantsIt, 2000)
+// }
+
+// var weWantsIt = function(){
+//   var gollum = $('<div id=gollum></div>')
+//   var theRing = $('div#the-ring')
+//   var mountDoom = $('div#mount-doom').eq(0)
+
+//   gollum.append(theRing)
+//   mountDoom.append(gollum)
+
+//   setTimeout(thereAndBackAgain, 2000)
+// }
+
+// var thereAndBackAgain = function(){
+//   var gollum = $('div#gollum')
+//   var theRing = $('div#the-ring')
+//   var hobbits = $('li.hobbit')
+//   var theShire = $('article.land').eq(0)
+//   var mordor = $('article.land').eq(2)
+
+//   gollum.remove()
+
+//   theShire.append(hobbits)
+//   mordor.attr('class', 'collapse')
+
+// }
 
 
 
