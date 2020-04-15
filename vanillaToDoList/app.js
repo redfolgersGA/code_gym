36572a7879269f0cc08ca968dcea2313fce5865b
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-04-15 03:19:47 -0400

    apiData is visually represented on the DOM via a div/li/img tag

--- vanillaToDoList/app.js
+++ vanillaToDoList/app.js
@@ -4,22 +4,30 @@ const foodLogButton = $('.foodLogButton')
 const foodLoggedDivContainer = $('.foodContainer')
 const foodLoggedUlTag = $('.foodList')
 
-
+// next step is to grab a div/ul/li and visually represent your APIdata on the DOM
 
 foodLogButton.on('click', function(e){
   e.preventDefault()
 
-  $.ajax({
-  type: "GET",
-  url: "https://api.edamam.com/api/food-database/parser?ingr=orange&nutrition-type=logging&",
-  success: function(data){
-    console.log(data)
-  }
+    $.ajax({
+      type: "GET",
+      url: "https://api.edamam.com/api/food-database/parser?ingr="+foodLogInput.val()+"&nutrition-type=logging&,
+      success: function(data){
+        const liTagForFood = $("<li></li>")
+        const imgTagForFoodLi = $("<img>")
 
-})
+        liTagForFood.text(data.text)
+        liTagForFood.append(imgTagForFoodLi)
+
+        imgTagForFoodLi.attr('src', data.parsed[0].food.image)
 
 
+        foodLoggedUlTag.append(liTagForFood)
+        console.log(data.parsed[0].food.image)
+        foodLoggedDivContainer.css({"border": "solid 1px", "height": "200px", "width": "200px"})
+      }
 
+    })
 
 })
 
