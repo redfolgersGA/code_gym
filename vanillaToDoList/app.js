8bd1dda2a1290b8e170589b42d5bd3f85ce2b100
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-04-18 07:19:14 -0400

    switching API used in app to nutrition analysis api

--- vanillaToDoList/app.js
+++ vanillaToDoList/app.js
@@ -5,6 +5,11 @@ const foodLoggedDivContainer = $('.foodContainer')
 const foodLoggedUlTag = $('.foodList')
 const body = $('body')
 
+var calories = 0
+var protein = 0
+var fats = 0
+var carbs = 0
+
 // next step is to grab a div/ul/li and visually represent your APIdata on the DOM
 
 foodLogButton.on('click', function(e){
@@ -12,54 +17,64 @@ foodLogButton.on('click', function(e){
 
     $.ajax({
       type: "GET",
-      url: "https://api.edamam.com/api/food-database/parser?ingr="+foodLogInput.val()+"&nutrition-type=logging&",
+      url: "https://api.edamam.com/api/nutrition-data?,
+      // url: "https://api.edamam.com/api/food-database/parser?ingr="+foodLogInput.val()+"&nutrition-type=logging&app_id=e8f6cb8b&app_key=f55817f083b2b4ff70ce048485ee9db2",
       success: function(data){
-        const foodNutrients = data.parsed[0].food.nutrients
-        console.log(foodNutrients)
-        const liTagForFood = $("<li></li>")
-        const description = $("<div class='description'></div")
-        const imageOfFood = $("<img class='foodImage'>")
-        const foodName = $('<p id="foodName"></p>')
-        const foodCalories = $('<p class="calories"></p>')
-        const foodProtein = $('<p class="protein"></p>')
-        const foodFats = $('<p class="fats"></p>')
-        const foodCarbs = $('<p class="carbs"></p>')
-        const foodFiber = $('<p class="fiber"></p>')
-        const totalsDiv = $('.totals')
+        console.log(data)
+        // const foodNutrients = data.parsed[0].food.nutrients
+        // console.log(data.hints[0].measures[2].uri)
+        // const liTagForFood = $("<li></li>")
+        // const description = $("<div class='description'></div")
+        // const imageOfFood = $("<img class='foodImage'>")
+        // const foodName = $('<p id="foodName"></p>')
+        // const foodCalories = $('<p class="calories"></p>')
+        // const foodProtein = $('<p class="protein"></p>')
+        // const foodFats = $('<p class="fats"></p>')
+        // const foodCarbs = $('<p class="carbs"></p>')
+        // const foodFiber = $('<p class="fiber"></p>')
+        // const totalsDiv = $('.totals')
 
-        foodLoggedDivContainer.append(liTagForFood)
+        // foodLoggedDivContainer.append(liTagForFood)
 
-        liTagForFood.append(description)
-        liTagForFood.append(imageOfFood)
-
-
-        foodName.text(data.text)
-        foodCalories.text("Calories " + foodNutrients.ENERC_KCAL)
-        foodProtein.text("Protein " + foodNutrients.PROCNT + " g")
-        foodFats.text("Total Fat " + foodNutrients.FAT)
-        foodCarbs.text("Total Carbs " + foodNutrients.CHOCDF)
+        // liTagForFood.append(description)
+        // liTagForFood.append(imageOfFood)
 
 
+        // foodName.text(data.text)
+        // foodCalories.text("Calories " + foodNutrients.ENERC_KCAL)
+        // foodProtein.text("Protein " + foodNutrients.PROCNT + " g")
+        // foodFats.text("Total Fat " + foodNutrients.FAT)
+        // foodCarbs.text("Total Carbs " + foodNutrients.CHOCDF)
 
-
-        description.append(foodName)
-        description.append(foodCalories)
-        description.append(foodProtein)
-        description.append(foodFats)
-        description.append(foodCarbs)
+        // calories = calories + foodNutrients.ENERC_KCAL
+        // protein = protein + foodNutrients.PROCNT
+        // fats = fats + foodNutrients.FAT
+        // carbs = carbs + foodNutrients.CHOCDF
 
 
 
+        // description.append(foodName)
+        // description.append(foodCalories)
+        // description.append(foodProtein)
+        // description.append(foodFats)
+        // description.append(foodCarbs)
 
-        imageOfFood.attr('src', data.parsed[0].food.image)
-        foodLoggedUlTag.append(liTagForFood)
-
-        totalsDiv.text(foodNutrients.ENERC_KCAL + "totals Div")
 
 
-        totalsDiv.removeClass("hide")
-        totalsDiv.addClass("show")
-        body.append(totalsDiv)
+
+        // imageOfFood.attr('src', data.parsed[0].food.image)
+        // foodLoggedUlTag.append(liTagForFood)
+
+        // totalsDiv.text("Total Calories " + calories)
+        // totalsDiv.append('<p>' + "Total Protein " + Math.round(protein) + "g" + '</p>')
+        // totalsDiv.append('<p>' + "Total Fats " + Math.round(fats) + "g" + '</p>')
+        // totalsDiv.append('<p>' + "Total Carbs " + Math.round(carbs) + "g" + '</p>')
+
+
+
+        // totalsDiv.removeClass("hide")
+        // totalsDiv.addClass("show")
+        // body.append(totalsDiv)
 
 
 
