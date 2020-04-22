c8fcbf39c8450ed00f872acc9a239b3e1c951f71
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-04-22 07:28:14 -0400

    jquery loaded, sending an ajax request to my api served by express app

---
+++ u2/d06/homework/express-app-five/public/script.js
@@ -0,0 +1,9 @@
+console.log('script.js is loaded')
+
+var test = "jquery is loaded"
+
+var body = $('body')
+
+body.append("<p>" + test + "</p>")
+
+// send a request to the file path you created with EXPRESS app
