943a94bba86828e03938ca546bd894fbebb39158
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-04-22 07:46:09 -0400

    rendered JSON received from express app to localhost to the DOM

--- u2/d06/homework/express-app-five/public/script.js
+++ u2/d06/homework/express-app-five/public/script.js
@@ -12,8 +12,29 @@ $.ajax({
   type: "GET",
   url: "/api/npmPackages",
   success: function(data){
-    console.log(data.packages)
+    console.log(data)
 
     body.append("<h1>" + data.title + "</h1>")
+    body.append("<h3>" + data.text + "</h3>")
+
+    data.packages.forEach(function(package){
+      var divForPackage = $('<div></div>')
+      var h5ForName = $('<h5></h5>')
+      var pForCat = $('<p></p>')
+      var imgForImg = $('<img>')
+
+      h5ForName.text(package.name)
+      pForCat.text(package.category)
+      imgForImg.attr('src', package.img)
+
+      divForPackage.append(h5ForName)
+      divForPackage.append(pForCat)
+      divForPackage.append(imgForImg)
+
+      body.append(divForPackage)
+
+
+
+    })
   }
 })
