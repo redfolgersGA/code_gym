d24051d445df81bca3068bed3c7572c11cd6c916
Emmanuel Paulino <emmanuel.paulino@baruchmail.cuny.edu>
2020-04-24 01:45:51 -0400

    solved for Fats

--- vanillaToDoList/app.js
+++ vanillaToDoList/app.js
@@ -4,6 +4,8 @@ const foodLogButton = $('.foodLogButton')
 const foodLoggedDivContainer = $('.foodContainer')
 const foodLoggedUlTag = $('.foodList')
 const body = $('body')
+var tableClass = $('.table')
+var tableBody = $('<tbody></tbody>')
 
 // ADDS UP TOTALS FOR THE DAY
 var calories = 0
@@ -12,19 +14,20 @@ var fats = 0
 var carbs = 0
 
 // next step is to grab a div/ul/li and visually represent your APIdata on the DOM
-
+var counter = 1;
 foodLogButton.on('click', function(e){
   e.preventDefault()
 
     $.ajax({
       type: "GET",
-      url: "https://api.edamam.com/api/nutrition-data?app_id=&ingr="+foodLogInput.val(),
-      // url: "https://api.edamam.com/api/food-database/parser?ingr="+foodLogInput.val()+"&nutrition-type=logging&app_id=",
+      url: "https://api.edamam.com/api/nutrition-data?&ingr="+foodLogInput.val(),
+      // url: "https://api.edamam.com/api/food-database/parser?ingr="+foodLogInput.val()+"&nutrition-type=logging&",
       success: function(data){
         console.log(data)
         const foodNutrients = data.totalNutrients
 
-        const liTagForFood = $("<li></li>")
+
+        const liTagForFood = $("<li class='list-group-item' id=" + counter + "></li>")
         const description = $("<div class='description'></div")
         // const imageOfFood = $("<img class='foodImage'>")
         const foodName = $('<p id="foodName"></p>')
@@ -34,23 +37,29 @@ foodLogButton.on('click', function(e){
         const foodCarbs = $('<p class="carbs"></p>')
         const foodFiber = $('<p class="fiber"></p>')
         const totalsDiv = $('.totals')
+        const deleteLiBttn = $('<button type="button" class="btn btn-primary deleteBttn" id=' + counter + '></button>')
 
         foodLoggedDivContainer.append(liTagForFood)
 
         liTagForFood.append(description)
+        liTagForFood.append(deleteLiBttn)
 
 
 
         foodName.text(foodLogInput.val())
-        foodCalories.text("Calories  " + data.calories)
-        foodProtein.text("Protein " + Math.round(foodNutrients.PROCNT.quantity) + " g")
-        foodFats.text("Total Fat " + Math.round(foodNutrients.FAT.quantity) + " g")
-        foodCarbs.text("Total Carbs " + Math.round(foodNutrients.CHOCDF.quantity) + " g")
+        foodCalories.text("Calories" + data.calories)
+        foodProtein.text("Protein" + Math.round(foodNutrients.PROCNT.quantity) + " g")
+        foodFats.text("Total Fat" + Math.round(foodNutrients.FAT.quantity) + " g")
+        foodCarbs.text("Total Carbs" + Math.round(foodNutrients.CHOCDF.quantity) + " g")
 
-        calories = calories + foodNutrients.ENERC_KCAL.quantity
-        protein = protein + foodNutrients.PROCNT.quantity
-        fats = fats + foodNutrients.FAT.quantity
-        carbs = carbs + foodNutrients.CHOCDF.quantity
+        calories = calories + Math.round(foodNutrients.ENERC_KCAL.quantity)
+        console.log(calories, 'calories')
+        protein = protein + Math.round(foodNutrients.PROCNT.quantity)
+        console.log(protein, 'protein')
+        fats = fats + Math.round(foodNutrients.FAT.quantity)
+        console.log(fats, 'fats')
+        carbs = carbs + Math.round(foodNutrients.CHOCDF.quantity)
+        console.log(carbs, 'carbs')
 
 
 
@@ -66,7 +75,8 @@ foodLogButton.on('click', function(e){
         // imageOfFood.attr('src', data.parsed[0].)
         foodLoggedUlTag.append(liTagForFood)
 
-        totalsDiv.text("Total Calories " + Math.floor(calories))
+        // YOU MAY HAVE TO MOVE THIS OUTSIDE OR INTO A FUNCTION
+        totalsDiv.text("Total Calories " + Math.round(calories))
         totalsDiv.append('<p>' + "Total Protein " + Math.round(protein) + "g" + '</p>')
         totalsDiv.append('<p>' + "Total Fats " + Math.round(fats) + "g" + '</p>')
         totalsDiv.append('<p>' + "Total Carbs " + Math.round(carbs) + "g" + '</p>')
@@ -76,11 +86,60 @@ foodLogButton.on('click', function(e){
         totalsDiv.removeClass("hide")
         totalsDiv.addClass("show")
         body.append(totalsDiv)
+
+        deleteLiBttn.on('click', deleteTheLi)
+        counter++
       }
 
     })
 
 })
+
+
+function deleteTheLi(e){
+  const deleteLiBttn = $('button.deleteBttn')
+  const liTagForFood = $('li.list-group-item')
+  const foodCalories = $('p.calories')
+  const foodProtein = $('p.protein')
+  const foodFats = $('p.fats')
+  const foodCarbs = $('p.carbs')
+  e.target.parentElement.remove()
+  // console.log(Math.round(fats), "is now")
+
+  console.log(fats)
+ fats = fats - foodFats.text().match(/\d+/g).map(Number)[0];
+  console.log(fats)
+
+ // console.log(convertFatStringToNumber)
+ // console.log(foodFats)
+
+
+// for(var i=0;i<convertFatStringToNumber.length;i++){
+//   foodFats
+// }
+//  console.log(convertFatStringToNumber[0])
+
+ // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+ // console.log(convertFatStringToNumber.reduce(reducer))
+
+
+
+ // fats = fats - convertFatStringToNumber.reduce(reducer)
+ // console.log(fats, "is now")
+
+
+ //  console.log(convertFatStringToNumber)
+
+
+
+
+
+
+
+}
+
+
+
 
 // THIS FEATURE WILL ALLOW USER TO SELECT WHAT TYPE OF FOOD WAS CONSUMED AND ADD AN IMAGE OF FOOD TO THE DOM
 // $.ajax({
