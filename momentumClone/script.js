c1f741900e411038d3a71573896741637e8ab980
Emmanuel Paulino <epaulino2016@gmail.com>
2020-06-02 09:16:16 -0400

    created clock using date object

--- momentumClone/script.js
+++ momentumClone/script.js
@@ -1,42 +1,108 @@
 console.log("script loaded")
-// query the DOM for my elements
-const time = $('span#time')
-console.log(time)
-// instantiate Date Object
-const dateNow = new Date();
-const minutes = dateNow.getMinutes()
 
-console.log(dateNow.getMinutes())
-
-function setSeconds(){
-  let seconds = setInterval(dateNow.getSeconds(),5000)
-  time.text(`${seconds}`)
+const time = document.getElementById('time')
 
 
-}
 
 function setTime(){
-  setSeconds()
+  let today = new Date()
+  let hours = today.getHours()
+  let minutes = 4
+  let seconds = today.getSeconds()
 
-  console.log('hey')
-  // setTimeout(setTime, 5000)
+  if(seconds < 9){
+    seconds = "0" + seconds;
+  }
+
+  if (minutes < 9){
+    minutes = "0" + minutes
+  }
+
+
+
+  time.textContent = `${hours} ${minutes} ${seconds}`
+
+
+
+
+
+  console.log(minutes)
+  console.log(today)
+
 }
 
-// let myTimer = setInterval(setTime, 1000)
+setInterval(setTime, 1000)
 
-// myTimer()
 
-var myVar;
 
-function callTime() {
-  myVar = setInterval(setTime, 5000);
-}
 
-// function alertFunc() {
-//   alert("Hello!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// query the DOM for my elements
+// const time = $('span#time')
+// console.log(time)
+// // instantiate Date Object
+// const dateNow = new Date();
+// const minutes = dateNow.getMinutes()
+// let seconds = setInterval(dateNow.getSeconds(), 1000)
+
+// console.log(dateNow.getMinutes())
+
+// function setSeconds(){
+//   // let minutes = setInterval(dateNow.getMinutes(), 60000)
+
+
+//   time.text(`${seconds}`)
+
+
+
 // }
 
-callTime()
+// setInterval(setSeconds, 1000)
+
+// function setTime(){
+//   setSeconds()
+
+//   console.log('hey')
+//   // setTimeout(setTime, 5000)
+// }
+
+// // let myTimer = setInterval(setTime, 1000)
+
+// // myTimer()
+
+// var myVar;
+
+// // function callTime() {
+// //   myVar = setInterval(setTime, 1000);
+// // }
+
+// // // function alertFunc() {
+// // //   alert("Hello!");
+// // // }
+
+// // callTime()
 
 
 
