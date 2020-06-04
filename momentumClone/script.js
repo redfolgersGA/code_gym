64249463ec9b46c2bc07ded830b358c89cbea3ce
Emmanuel Paulino <epaulino2016@gmail.com>
2020-06-04 12:52:27 -0400

    show focus span, hide focus span

--- momentumClone/script.js
+++ momentumClone/script.js
@@ -9,41 +9,30 @@ const spanFirstName = $('span#firstName')
 let timeOfDay = ""
 let greetingSpan = $('span#greeting')
 
+let focusSpan = $('span#focus')
+let inputFocus = $('input#inputFocus')
+let focusButton = $('button#submitFocus')
 
 
 
+$.ajax({
+  type: "GET",
+  url: "https://quotes.rest/qod",
+  method: "GET",
+  success: function(data){
+    console.log(data.contents.quotes[0].quote)
+    pTagForQuote.text(data.contents.quotes[0].quote)
 
+    // if(data.quote.length < 105){
+    //   pTagForQuote.text(data.quote)
 
+    // } else{
+    //   console.log("more than 105")
+    // }
 
-
-nameButton.on("click", captureName)
-
-function captureName(e){
-  e.preventDefault()
-  let today = new Date()
-  var nameInputValue = nameInput.val()
-
-  if(nameInputValue === ""){
-    console.log('its empty dude')
-  } else {
-    console.log(nameInputValue)
-
-    spanFirstName.text(nameInputValue)
+    // console.log(data.quote.length)
   }
-
-  if(today.toLocaleString("en-US").includes('AM')){
-   greetingSpan.text("Good Morning")
-    } else {
-      greetingSpan.text("Good Evening")
-    }
-
-
-
-
-}
-
-
-
+})
 
 function setTime(){
   let today = new Date()
@@ -70,51 +59,44 @@ setInterval(setTime, 0)
 
 
 
+nameButton.on("click", captureName)
 
+function captureName(e){
+  e.preventDefault()
+  let today = new Date()
+  let nameInputValue = nameInput.val()
 
-$.ajax({
-  type: "GET",
-  url: "https://quotes.rest/qod",
-  method: "GET",
-  success: function(data){
-    console.log(data.contents.quotes[0].quote)
-    pTagForQuote.text(data.contents.quotes[0].quote)
+  if(nameInputValue === ""){
+    console.log('its empty dude')
+  } else {
+    console.log(nameInputValue)
 
-    // if(data.quote.length < 105){
-    //   pTagForQuote.text(data.quote)
-
-    // } else{
-    //   console.log("more than 105")
-    // }
-
-    // console.log(data.quote.length)
+    spanFirstName.text(nameInputValue)
   }
-})
 
-// $.ajax({
-//   type: "GET",
-//   url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
-//   method: "GET",
-//   success: function(data){
-//    // const response = JSON.parse(data)
-//    console.log(data[2])
-//   }
+  if(today.toLocaleString("en-US").includes('AM')){
+   greetingSpan.text("Good Morning")
+    } else {
+      greetingSpan.text("Good Evening")
+    }
+}
 
-// })
+focusButton.on('click', captureFocus)
+
+function captureFocus(e){
+  e.preventDefault()
+  let focusCaptured = inputFocus.val()
+  console.log(focusCaptured, "focus captured")
 
 
-// const settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://type.fit/api/quotes",
-//   "method": "GET"
-// }
+// hide focus span and show a div with the main focus displayed as a checklist item
+  focusSpan.hide()
+  inputFocus.hide()
+  focusButton.hide()
 
-// $.ajax(settings).done(function (response) {
-//   const data = JSON.parse(response);
-//   console.log(data);
-// });
 
+
+}
 
 
 
@@ -127,64 +109,6 @@ $.ajax({
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// query the DOM for my elements
-// const time = $('span#time')
-// console.log(time)
-// // instantiate Date Object
-// const dateNow = new Date();
-// const minutes = dateNow.getMinutes()
-// let seconds = setInterval(dateNow.getSeconds(), 1000)
-
-// console.log(dateNow.getMinutes())
-
-// function setSeconds(){
-//   // let minutes = setInterval(dateNow.getMinutes(), 60000)
-
-
-//   time.text(`${seconds}`)
-
-
-
-// }
-
-// setInterval(setSeconds, 1000)
-
-// function setTime(){
-//   setSeconds()
-
-//   console.log('hey')
-//   // setTimeout(setTime, 5000)
-// }
-
-// // let myTimer = setInterval(setTime, 1000)
-
-// // myTimer()
-
-// var myVar;
-
-// // function callTime() {
-// //   myVar = setInterval(setTime, 1000);
-// // }
-
-// // // function alertFunc() {
-// // //   alert("Hello!");
-// // // }
-
-// // callTime()
 
 
 
