9bbaf241831132ade019922305b7ff1629093043
Emmanuel Paulino <epaulino2016@gmail.com>
2020-06-04 11:27:47 -0400

    pulling data from inspirational quote api

--- momentumClone/script.js
+++ momentumClone/script.js
@@ -1,6 +1,47 @@
 console.log("script loaded")
 
 const time = document.getElementById('time')
+const pTagForQuote = $('#quote')
+let nameInput = $('input#inputName')
+// let nameInputValue = nameInput.val()
+const nameButton = $('#submitName')
+const spanFirstName = $('span#firstName')
+let timeOfDay = ""
+let greetingSpan = $('span#greeting')
+
+
+
+
+
+
+
+
+nameButton.on("click", captureName)
+
+function captureName(e){
+  e.preventDefault()
+  let today = new Date()
+  var nameInputValue = nameInput.val()
+
+  if(nameInputValue === ""){
+    console.log('its empty dude')
+  } else {
+    console.log(nameInputValue)
+
+    spanFirstName.text(nameInputValue)
+  }
+
+  if(today.toLocaleString("en-US").includes('AM')){
+   greetingSpan.text("Good Morning")
+    } else {
+      greetingSpan.text("Good Evening")
+    }
+
+
+
+
+}
+
 
 
 
@@ -10,11 +51,11 @@ function setTime(){
   let minutes = today.getMinutes()
   let seconds = today.getSeconds()
 
-  if(seconds < 9){
+  if(seconds <= 9){
     seconds = "0" + seconds;
   }
 
-  if (minutes < 9){
+  if (minutes <= 9){
     minutes = "0" + minutes
   }
 
@@ -22,16 +63,57 @@ function setTime(){
 
   time.textContent = `${hours} ${minutes} ${seconds}`
 
-
-  console.log(minutes)
-  console.log(today)
-
 }
 
-setInterval(setTime, 1000)
+setInterval(setTime, 0)
 
 
 
+
+
+
+$.ajax({
+  type: "GET",
+  url: "https://quotes.rest/qod",
+  method: "GET",
+  success: function(data){
+    console.log(data.contents.quotes[0].quote)
+    pTagForQuote.text(data.contents.quotes[0].quote)
+
+    // if(data.quote.length < 105){
+    //   pTagForQuote.text(data.quote)
+
+    // } else{
+    //   console.log("more than 105")
+    // }
+
+    // console.log(data.quote.length)
+  }
+})
+
+// $.ajax({
+//   type: "GET",
+//   url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+//   method: "GET",
+//   success: function(data){
+//    // const response = JSON.parse(data)
+//    console.log(data[2])
+//   }
+
+// })
+
+
+// const settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://type.fit/api/quotes",
+//   "method": "GET"
+// }
+
+// $.ajax(settings).done(function (response) {
+//   const data = JSON.parse(response);
+//   console.log(data);
+// });
 
 
 
